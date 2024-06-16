082a43da1e1675553720050b8e254263b374f71b
Linh Nguyen <mlinhng@gmail.com>
2024-06-16 19:08:35 +0200

    created a transaction to create a new paragraph

--- data/posts.go
+++ data/posts.go
@@ -9,7 +9,7 @@ type PostStore interface {
 	Create(title string) (int, error)
 	GetById(id int) (title string, err error)
 	PutTitle(post types.Post) (types.Post, error)
-	PostContent(id int, orderInPost int) (types.Content, error)
+	CreatePostContent(id int, orderInPost int) (*types.Content, error)
 }
 
 type PostModel struct {
@@ -35,12 +35,34 @@ func (model *PostModel) PutTitle(post types.Post) (types.Post, error) {
 	return post, err
 }
 
-func (model *PostModel) PostContent(id int, orderInPost int) (types.Content, error) {
+func (model *PostModel) CreatePostContent(postId int, orderInPost int) (*types.Content, error) {
+	paragraph := &types.Content{
+		Type:  types.ContentParagraphs,
+		Value: "",
+	}
+
+	// transaction
+	transaction, err := model.Store.Begin()
+	if err != nil {
+		return nil, err
+	}
+	defer transaction.Rollback()
 	// insert into posts_contents -> content_id
+	var contentId int
+	var paragraphId int
 
+	transaction.QueryRow(`INSERT INTO posts_contents(content_type, post_id, order_in_post) VALUES ($1, $2, $3) RETURNING content_id`, types.ContentParagraphs, postId, orderInPost).Scan(&contentId)
 	// insert into paragraphs
+	transaction.QueryRow(`INSERT INTO paragraphs(content_id, value) VALUES ($1, $2)`, contentId, "").Scan(&paragraphId)
 
-	return types.Content{}, nil
+	paragraph.ContentId = contentId
+	paragraph.Id = paragraphId
+
+	if err = transaction.Commit(); err != nil {
+		return nil, err
+	}
+
+	return paragraph, nil
 }
 
 // func (model *PostModel) GetAllContents() ([]types.Content, error) {
